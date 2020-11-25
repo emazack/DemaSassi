2ed308602c48b8a87c2a337e00aa84ccdfe3f565
emazack <YB050@YOURBIZ.LAN>
2020-11-25 17:56:58 +0100

    aggiunti file

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // to prevent jQuery to run before the document is ready (fully loaded)
 $(document).ready(function(){
+    
+    $('.middle-header').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
 
-    console.log('ciao');
-  
+      
 });
 
