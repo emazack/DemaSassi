b1aafa9490490cda3f1e77747f2e81ed31c83431
emanuele <em.zaccaria@gmail.com>
2020-11-26 12:46:53 +0100

    Situazione slider strambo

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // to prevent jQuery to run before the document is ready (fully loaded)
 $(document).ready(function(){
     
-    $('.middle-header').slick({
+    $('.header-slider').slick({
         dots: true,
         infinite: true,
         speed: 500,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),        
       });
 
       
