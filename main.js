d2e4e4f3e836ba33062955c6ea75755ed471fffb
emazack <YB050@YOURBIZ.LAN>
2020-12-04 17:35:29 +0100

    Js hamburger working

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 // to prevent jQuery to run before the document is ready (fully loaded)
-$(document).ready(function(){
-    
-    $('.main-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-        appendDots: $('.bottom-header'),         
-      });
+document.addEventListener('DOMContentLoaded', initWebPageJS);
+const menuHamburgerIcon = document.querySelector(".hamburger");
 
-       
-});
+function initWebPageJS() {
+
+  const isMobile = checkMobile();
+  initSlick();
+  isMobile && initMenuBehaviour();
+
+}
+
+function initSlick() {
+
+  $('.main-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    appendDots: $('.bottom-header'),
+  });
+
+}
+
+function initMenuBehaviour() {
+
+  const menu = document.querySelector(".menu");
+
+  menuHamburgerIcon.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  })
+
+}
+
+function checkMobile() {
+
+  return window.innerWidth <= 768;
+
+}
 
